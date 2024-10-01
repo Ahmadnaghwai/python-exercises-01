68d1d87e03889346629d55c53f80cb4d3e58785e
Ahmad <omninaghwai@gmail.com>
2024-10-01 14:52:30 +0200

    Übungsaufgabe 1 abgeschlossen

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# python-exercises-01
+# Python Exercises 01
+
+## Übungsaufgabe 1
+- Dieses Skript definiert Variablen für Vorname, Nachname, Geburtsjahr und Führerscheinstatus und gibt diese aus.
---
+++ exercise_1.py
@@ -0,0 +1,13 @@
+# exercise_1.py
+
+# Variablen definieren
+first_name = "Ahmad"  # Ersetze mit deinem Vornamen
+last_name = "Naghwai"   # Ersetze mit deinem Nachnamen
+birth_year = 1991            # Ersetze mit deinem Geburtsjahr
+has_license = False             # Ersetze mit True oder False
+
+# Ausgabe der Variablenwerte
+print("Vorname:", first_name)
+print("Nachname:", last_name)
+print("Geburtsjahr:", birth_year)
+print("Hat Führerschein:", has_license)
